36310cccbcec75f8b9f863f4dd589a44bf4c3b7a
Edgar J. Salas <ejsalasdev@gmail.com>
2024-04-25 20:53:23 -0500

    add preliminary auth code

--- routes/auth.py
+++ routes/auth.py
@@ -1,6 +1,42 @@
-from datetime import timedelta, datetime
-from fastapi import APIRouter
+from datetime import timedelta, datetime, timezone
+from fastapi import APIRouter, Depends, HTTPException
+from starlette import status
 from admin import pwd_context
+from jose import jwt, JWTError
+from fastapi.security import OAuth2PasswordBearer
 
 
 router = APIRouter(prefix="/auth", tags=["auth"])
+
+SECRET_KEY = "41Hcg7wVEtauHAvQp3Veaopizpk+NqCSacwYGKaK7Ng="
+ALGORITHM = "HS256"
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(days=1)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def verify_token(token: str):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        return username
+    except JWTError:
+        raise credentials_exception
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    return verify_token(token)
